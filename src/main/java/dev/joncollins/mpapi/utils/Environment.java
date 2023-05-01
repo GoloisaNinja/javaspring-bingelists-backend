33fef43caa945947b6f54331885fbcfa6f749ce3
Golodhrim <holidayninjastaff@gmail.com>
2023-05-01 15:45:07 -0400

    docker updates

--- src/main/java/dev/joncollins/mpapi/utils/Environment.java
+++ src/main/java/dev/joncollins/mpapi/utils/Environment.java
@@ -3,5 +3,5 @@ package dev.joncollins.mpapi.utils;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public interface Environment {
-    Dotenv dotenv = Dotenv.load();
+    //Dotenv dotenv = Dotenv.load();
 }
